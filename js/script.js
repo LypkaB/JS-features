65814c442b59b3147e7d9f869f123607f3b3bf16
LypkaB <lpk.bdn@gmail.com>
2020-07-22 19:00:00 +0300

    Game «Ball»

--- js/script.js
+++ js/script.js
@@ -1,31 +1,115 @@
-let clicks = 0;
-const TIMEOUT = 5000,
-      display = document.querySelector('#display'),
-      button = document.querySelector('#button'),
-      counter = document.querySelector('#counter');
+const character = document.getElementById('character'),
+      game = document.getElementById('game');
+      currentBlocks = [];
 
-button.onclick = start;
+let interval,
+    both = 0,
+    counter = 0;
 
-function start() {
-    const startTime = Date.now();
+function moveLeft() {
+    const left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
 
-    display.textContent = formatTime(TIMEOUT);
-    button.onclick = () => counter.textContent = clicks++;
-
-    const interval = setInterval(() => {
-        const delta = Date.now() - startTime;
-        display.textContent = formatTime(TIMEOUT - delta);
-    }, 100);
-
-    const timeout = setTimeout(() => {
-        button.onclick = null;
-        display.textContent = 'Game Over';
-
-        clearInterval(interval);
-        clearTimeout(timeout);
-    }, TIMEOUT);
+    if (left > 0) {
+        character.style.left = left - 2 + 'px';
+    }
 }
 
-function formatTime(ms) {
-    return Number.parseFloat(ms/1000).toFixed(2);
+function moveRight() {
+    const left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+
+    if (left < 380) {
+        character.style.left = left + 2 + 'px';
+    }
 }
+
+document.addEventListener("keydown", event => {
+    if (both == 0) {
+        both++;
+
+        if (event.key === 'ArrowLeft') {
+            interval = setInterval(moveLeft, 1);
+        }
+        if (event.key === 'ArrowRight') {
+            interval = setInterval(moveRight, 1);
+        }
+    }
+});
+
+document.addEventListener('keyup', event => {
+    clearInterval(interval);
+    both = 0;
+});
+
+const blocks = setInterval(function() {
+    const blockLast = document.getElementById('block' + (counter - 1)),
+          holeLast = document.getElementById('hole' + (counter - 1));
+
+    if (counter > 0) {
+        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue('top')),
+            holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue('top'));
+    }
+    
+    if (blockLastTop < 400 || counter == 0) {
+        let block = document.createElement('div'),
+            hole = document.createElement('div');
+        
+        block.setAttribute('class', 'block');
+        hole.setAttribute('class', 'hole');
+        block.setAttribute('id', 'block' + counter);
+        hole.setAttribute('id', 'hole' + counter);
+
+        block.style.top = blockLastTop + 100 + 'px';
+        hole.style.top = holeLastTop + 100 + 'px';
+
+        let random = Math.floor(Math.random() * 360);
+
+        hole.style.left = random + 'px';
+
+        game.appendChild(block);
+        game.appendChild(hole);
+        currentBlocks.push(counter);
+        counter++;
+    }
+
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top')),
+        characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left')),
+        drop = 0;
+
+    if (characterTop <= 0) {
+        alert('Game over. Score: ' + (counter - 9));
+        clearInterval(blocks);
+        location.reload();
+    }
+
+    for (let i = 0; i < currentBlocks.length; i++) {
+        let current = currentBlocks[i],
+            iblock = document.getElementById('block' + current),
+            ihole = document.getElementById('hole' + current),
+            iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue('top')),
+            iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue('left'));
+
+        iblock.style.top = iblockTop - 0.5 + 'px';
+        ihole.style.top = iblockTop - 0.5 + 'px';
+
+        if (iblockTop < -20) {
+            currentBlocks.shift();
+            iblock.remove();
+            ihole.remove();
+        }
+
+        if (iblockTop - 20 < characterTop && iblockTop > characterTop) {
+            drop++;
+
+            if (iholeLeft <= characterLeft && iholeLeft + 20 >= characterLeft) {
+                drop = 0;
+            }
+        }
+    }
+    if (drop == 0) {
+        if (characterTop < 480) {
+            character.style.top = characterTop + 2 + 'px';
+        }
+    } else {
+        character.style.top = characterTop - 0.5 + 'px';
+    }
+}, 1);
