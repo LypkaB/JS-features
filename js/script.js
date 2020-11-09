88242312797f3c7b0ccae4344cf48c23cb5a8f24
LypkaB <lpk.bdn@gmail.com>
2020-11-09 19:00:00 +0200

    Todo List («MVC» pattern)

--- js/script.js
+++ js/script.js
@@ -1,115 +1,83 @@
-const character = document.getElementById('character'),
-      game = document.getElementById('game');
-      currentBlocks = [];
+model = {
+    items: []
+};
 
-let interval,
-    both = 0,
-    counter = 0;
+view = {
+    clearList: function() {
+        const range = document.createRange();
 
-function moveLeft() {
-    const left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+        range.selectNodeContents(document.getElementById('list'));
+        range.deleteContents();
+    },
+    render: function() {
+        this.clearList();
 
-    if (left > 0) {
-        character.style.left = left - 2 + 'px';
-    }
-}
+        if (model.items.length != 0) {
+            list = document.getElementById('list');
 
-function moveRight() {
-    const left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+            for (let i = 0; i < model.items.length; i++) {
+                item      = document.createElement('li');
+                span      = document.createElement('span');
+                check     = document.createElement('a');
+                cross     = document.createElement('a');
+                iconCheck = document.createElement('i');
+                iconCross = document.createElement('i');
 
-    if (left < 380) {
-        character.style.left = left + 2 + 'px';
-    }
-}
+                item.className = 'item';
+                span.className = 'item-text';
+                check.className = 'item-complete';
+                cross.className = 'item-delete';
 
-document.addEventListener("keydown", event => {
-    if (both == 0) {
-        both++;
+                span.textContent = model.items[i].text;
 
-        if (event.key === 'ArrowLeft') {
-            interval = setInterval(moveLeft, 1);
+                if (model.items[i].completed) span.setAttribute('style', 'text-decoration: line-through; color: #bbb');
+
+                iconCheck.setAttribute('class', 'icon ion-md-checkmark');
+                iconCheck.setAttribute('data-id', i);
+                iconCross.setAttribute('class', 'icon ion-md-trash');
+                iconCross.setAttribute('data-id', i);
+
+                check.setAttribute("onclick", "controller.completeItem('" + i + "')");
+                cross.setAttribute("onclick", "controller.deleteItem('" + i + "')");
+
+                check.appendChild(iconCheck);
+                cross.appendChild(iconCross);
+                item.appendChild(span);
+                item.appendChild(check);
+                item.appendChild(cross);
+
+                list.appendChild(item);
+            }
         }
-        if (event.key === 'ArrowRight') {
-            interval = setInterval(moveRight, 1);
-        }
-    }
-});
+    },
+    addItem: function(e) {
+        if (e.code == 'Enter') {
+            input = document.getElementById('add-item');
 
-document.addEventListener('keyup', event => {
-    clearInterval(interval);
-    both = 0;
-});
-
-const blocks = setInterval(function() {
-    const blockLast = document.getElementById('block' + (counter - 1)),
-          holeLast = document.getElementById('hole' + (counter - 1));
-
-    if (counter > 0) {
-        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue('top')),
-            holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue('top'));
-    }
-    
-    if (blockLastTop < 400 || counter == 0) {
-        let block = document.createElement('div'),
-            hole = document.createElement('div');
-        
-        block.setAttribute('class', 'block');
-        hole.setAttribute('class', 'hole');
-        block.setAttribute('id', 'block' + counter);
-        hole.setAttribute('id', 'hole' + counter);
-
-        block.style.top = blockLastTop + 100 + 'px';
-        hole.style.top = holeLastTop + 100 + 'px';
-
-        let random = Math.floor(Math.random() * 360);
-
-        hole.style.left = random + 'px';
-
-        game.appendChild(block);
-        game.appendChild(hole);
-        currentBlocks.push(counter);
-        counter++;
-    }
-
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top')),
-        characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left')),
-        drop = 0;
-
-    if (characterTop <= 0) {
-        alert('Game over. Score: ' + (counter - 9));
-        clearInterval(blocks);
-        location.reload();
-    }
-
-    for (let i = 0; i < currentBlocks.length; i++) {
-        let current = currentBlocks[i],
-            iblock = document.getElementById('block' + current),
-            ihole = document.getElementById('hole' + current),
-            iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue('top')),
-            iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue('left'));
-
-        iblock.style.top = iblockTop - 0.5 + 'px';
-        ihole.style.top = iblockTop - 0.5 + 'px';
-
-        if (iblockTop < -20) {
-            currentBlocks.shift();
-            iblock.remove();
-            ihole.remove();
-        }
-
-        if (iblockTop - 20 < characterTop && iblockTop > characterTop) {
-            drop++;
-
-            if (iholeLeft <= characterLeft && iholeLeft + 20 >= characterLeft) {
-                drop = 0;
+            if ((input.value != '') || (input.value != ' ')) {
+                controller.addItem(input.value);
+                return false;
             }
         }
     }
-    if (drop == 0) {
-        if (characterTop < 480) {
-            character.style.top = characterTop + 2 + 'px';
-        }
-    } else {
-        character.style.top = characterTop - 0.5 + 'px';
+};
+
+controller = {
+    init: function() {
+        view.render();
+    },
+    addItem: function(item) {
+        list_item = { text: item, completed: false };
+        model.items.push(list_item);
+        document.getElementById('add-item').value = "";
+        view.render();
+    },
+    completeItem: function(item_index) {
+        model.items[item_index].completed = !model.items[item_index].completed;
+        view.render();
+    },
+    deleteItem: function(item_index) {
+        model.items.splice(item_index, 1);
+        view.render();
     }
-}, 1);
+};
