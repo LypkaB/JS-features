db5663ff14f63f07946c04724e7325946d0b02b0
LypkaB <lpk.bdn@gmail.com>
2022-12-10 08:00:00 +0200

    Collapse/expand function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('table').addEventListener('change', ({ target: t }) => {
-        t.closest('tr').querySelectorAll('input[type="radio"]').forEach(n => n.checked = n === t);
-    });
-})
+const descriptionTextFunctions = (function () {
+    const getElements = () => {
+        const dtTable = document.querySelector('.description_text');
+        const dtMax = document.querySelector('.description_text-maximize-icon');
+        const dtMin = document.querySelector('.description_text-minimize-icon');
+        return [dtTable, dtMax, dtMin];
+    }
+
+    const expand = () => {
+        const [dtTable, dtMax, dtMin] = getElements();
+        dtMax.addEventListener('click', function () {
+            dtTable.classList.remove('description_collapse');
+            dtMax.classList.add('icon-hidden');
+            dtMin.classList.remove('icon-hidden');
+        })
+    }
+
+    const collapse = () => {
+        const [dtTable, dtMax, dtMin] = getElements();
+    }
+
+    return {
+        expand,
+        collapse
+    }
+}());
