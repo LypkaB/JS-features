878bc69420f58e0f5ab9eaa91fef478eacc57801
LypkaB <lpk.bdn@gmail.com>
2022-12-28 20:00:00 +0200

    Textarea auto height

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.querySelectorAll('textarea').forEach(el => {
+    el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+    el.classList.add('auto');
+    el.addEventListener('input', e => {
+        el.style.height = 'auto';
+        el.style.height = (el.scrollHeight) + 'px';
+    });
+});
